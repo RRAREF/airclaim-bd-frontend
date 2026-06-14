e0f66a06750004eeca7ac481b52d80f13c6cf561
RRAREF <rubaiataref.cse@gmail.com>
2026-06-14 23:38:12 +0600

    Stage 2 complete frontend

--- airclaim-backend/src/main/java/com/airclaimbd/airclaimbackend/repository/FoundItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.airclaimbd.airclaimbackend.repository;
-
-import com.airclaimbd.airclaimbackend.entity.FoundItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoundItemRepository extends JpaRepository<FoundItem, Long> {
-}
